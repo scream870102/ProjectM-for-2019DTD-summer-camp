3a6cd8af16c0584f24fab009e9b383506a574e97
Scream <scream870102@gmail.com>
2019-05-04 22:51:14 +0800

    FinalVer

---
+++ Assets/Script/Not for demo/Death.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour {
+    private void OnCollisionEnter2D (Collision2D other) {
+        if (other.gameObject.tag == "Enemy") {
+            SceneManager.LoadScene (0);
+        }
+    }
+}
---
+++ Assets/Script/Not for demo/Yee.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Yee : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D rb;
+    bool bFacingRight;
+    void Start ( ) {
+        bFacingRight = false;
+    }
+
+    void Update ( ) {
+        if (rb != null) {
+            Vector2 movement = bFacingRight?Vector2.right : Vector2.left;
+            rb.velocity = movement * speed * Time.deltaTime;
+        }
+
+    }
+
+    private void OnTriggerExit2D (Collider2D other) {
+        if (other.gameObject.tag == "Ground")
+            Flip ( );
+    }
+    
+    void Flip ( ) {
+        bFacingRight = !bFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
